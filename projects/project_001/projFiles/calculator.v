16685ff8800fb8c89b9e6209613b59e7c9baf2d4
immoghul <immoghul@ncsu.edu>
2022-03-04 16:51:29 -0500

    partially switched to structural

--- projects/project_001/projFiles/calculator.v
+++ projects/project_001/projFiles/calculator.v
@@ -7,7 +7,14 @@ module calculator(
 	wire [5:0] negCpD;
 	fourBitAdder u1(A,B,ApB);
 	fourBitAdder u2(C,D,CpD);
-	assign invCpD = ~CpD;
+	//assign invCpD = ~CpD;
+	
+	not(invCpD[0],CpD[0]);
+	not(invCpD[1],CpD[1]);
+	not(invCpD[2],CpD[2]);
+	not(invCpD[3],CpD[3]);
+	not(invCpD[4],CpD[4]);
+
 	fiveBitAdder u3(invCpD, 5'b00001, negCpD[4:0]);
 	fiveBitAdder u4(negCpD[4:0],ApB,F);	
 
