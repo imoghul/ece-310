7fb75b6fb0b3de7ff1438d509fec51fdbc2210c5
immoghul <immoghul@ncsu.edu>
2022-03-08 12:13:09 -0500

    updated lab 6 alarm assignment

--- labs/lab_005/alarm/lab_006_alarm.v
+++ labs/lab_005/alarm/lab_006_alarm.v
@@ -4,11 +4,31 @@ module lab_006_alarm (
   input [2:0] windows,
   output reg secure, alarm
 );
-
   always @* begin
-    if( alarm_set )
-
-    // fill out the rest
+    if( alarm_set ) begin
+       if(alarm_stay)
+	   if(|doors || |windows) begin
+		alarm<=1;
+		secure<=0;
+	   end
+           else begin
+		 alarm<=0;
+		 secure<=1;
+	   end
+       else
+           if(|windows) begin
+                alarm<=1;
+                secure<=0;
+           end
+           else begin 
+                 alarm<=0;
+                 secure<=1;
+           end
+    end
+    else begin
+       alarm <= 0;
+       secure<=1;
+    end
 
   end
 
