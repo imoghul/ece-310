54007db773b0a97c4b8ef704b6b4dc7063adc6f3
immoghul <immoghul@ncsu.edu>
2022-03-08 12:13:09 -0500

    added accumulator for lab 6

--- labs/lab_005/acc/lab_006_acc.v
+++ labs/lab_005/acc/lab_006_acc.v
@@ -8,7 +8,10 @@ module lab_006_acc (
     if( !rst_n ) begin
       d_out <= 0;
     end
-    
+    else begin
+      if(d_in>6) d_out <= d_out+d_in;
+      else d_out<=d_out;
+    end 
     // fill out the rest
 
   end
