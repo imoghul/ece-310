2e0958a831484b62bbb0c5ed9ad16cd79118944c
immoghul <immoghul@ncsu.edu>
2022-04-18 14:41:42 -0400

    added project 2

---
+++ projects/project_002/adler32.v
@@ -0,0 +1,16 @@
+module adler32(
+	input clock,rst_n,size_valid,data_start,
+	input [31:0] size,
+	input [7:0] data,
+	output checksum_valid,
+	output[31:0] checksum
+);
+	wire last;
+	wire [31:0] curr;
+	wire retrieving;
+	reg inProgress,onLast;
+	size_count u1(clock,rst_n,size_valid,size+1,data_start,last,retrieving,curr);
+	adler32_acc u2(rst_n,clock,retrieving,data,checksum);
+	assign checksum_valid = last;
+
+endmodule
